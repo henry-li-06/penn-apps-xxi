23270f55756589648e411269090fc0e13f1265c5
henryli6 <henry8254@gmail.com>
2020-09-12 17:33:38 -0400

    elimnate errors from API calls

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from utils import Model
 
 app = Flask(__name__)
 
+MAX_ATTEMPTS = 10
 
 @app.route('/', methods = ['POST'])
 def generate_lyrics():
@@ -10,8 +11,12 @@ def generate_lyrics():
         data = request.get_json()
         usr_input = data.get('text')
         artist = data.get('rapper')
-        generated_lyrics = Model.generate_seq(artist, usr_input)
-        return { 'generated_lyrics' : generated_lyrics }
+        for _ in range(MAX_ATTEMPTS):
+            try:
+                generated_lyrics = Model.generate_seq(artist, usr_input)
+                return { 'generated_lyrics' : generated_lyrics }
+            except:
+                pass
     except:
         return { 'message' : 'There was an error generating lyrics' }, 500
 
